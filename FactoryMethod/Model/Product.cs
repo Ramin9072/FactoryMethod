f30a0bcb41efb9861cfb762eaa3c3b4b79ba1fdb
r.navabian <r.navabian@NAVABIAN>
2023-07-09 14:01:10 +0330

    Add Product factory - builder

---
+++ FactoryMethod/Model/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethod.Model
+{
+    public sealed class Product
+    {
+        private Product(decimal retailPrice, string productName)
+        {
+            RetailPrice = retailPrice;
+            ProductName = productName;
+        }
+
+        public decimal RetailPrice { get; }
+        public string ProductName { get; }
+
+        public class Builder
+        {
+            private decimal _retailPrice;
+            private string _productName;
+
+            public Builder ProductName(string productName)
+            {
+                _productName = productName;
+                return this;
+            }
+
+            public Builder RetailPrice(decimal retailPrice)
+            {
+                _retailPrice = retailPrice;
+                return this;
+            }
+
+            public Product Create()
+            {
+                return new Product(_retailPrice, _productName);
+            }
+        }
+
+    }
+}
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -9,6 +9,17 @@ public class Program
         //var payment = Payment.ForChargingWallet();
         //var payment1 = Payment.ForPayingOrder();
 
+        var currentProduct = new Product.Builder()
+             .ProductName("Microphone")
+             .RetailPrice(200M)
+             .Create();
+
+        var currentProduct2 = new Product.Builder()
+              .ProductName("Microphone")
+              .Create();
+
+        Console.WriteLine(currentProduct.ProductName);
+
         var order = new Order(1000);
         Payment.ForPayingOrder(order);
 
